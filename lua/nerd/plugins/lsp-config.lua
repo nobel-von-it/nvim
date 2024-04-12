5c2e11ec246b33645c29409ebeefb7402383acfb
nobel-von-it <maksimdavydenko12@gmail.com>
2024-04-12 20:06:07 +0700

    make: normal lsp-config (testing in rust)

--- lua/nerd/plugins/lsp-config.lua
+++ lua/nerd/plugins/lsp-config.lua
@@ -3,19 +3,19 @@ return {
 	config = function()
 		-- Setup language servers.
 		local lspconfig = require('lspconfig')
-		lspconfig.pyright.setup {}
-		lspconfig.tsserver.setup {}
-		lspconfig.rust_analyzer.setup {
-			-- Server-specific settings. See `:help lspconfig-setup`
-			settings = {
-				['rust-analyzer'] = {},
-			},
-		}
+		local lspservers = {'rust_analyzer', 'pyright', 'tsserver'}
+		local root_dir = function()
+			return vim.fn.getcwd()
+		end
 
-
+		for _, lsp in pairs(lspservers) do
+			lspconfig[lsp].setup {
+				root_dir = root_dir
+			}
+		end
 		-- Global mappings.
 		-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-		vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+		vim.keymap.set('n', '<space>di', vim.diagnostic.open_float)
 		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 		vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 		vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
