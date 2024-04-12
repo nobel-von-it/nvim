708a405f0f85582a493238b55c4d6edafe9f8256
nobel-von-it <maksimdavydenko12@gmail.com>
2024-04-12 18:31:06 +0700

    add: numbers

--- lua/nerd/mappings.lua
+++ lua/nerd/mappings.lua
@@ -3,3 +3,8 @@ local vk = vim.keymap
 vk.set("i", "jk", "<ESC>", {})
 vk.set("n", "<leader>e", ":Ex<CR>", {})
 vk.set("n", "<leader>n", ":nohl<CR>", {})
+
+vk.set("n", "<C-h>", "<C-w>h", {})
+vk.set("n", "<C-j>", "<C-w>j", {})
+vk.set("n", "<C-k>", "<C-w>k", {})
+vk.set("n", "<C-l>", "<C-w>l", {})
--- lua/nerd/settings.lua
+++ lua/nerd/settings.lua
@@ -1,9 +1,17 @@
 local opt = vim.opt
 
+-- display left number
 opt.number = true
 opt.relativenumber = true
 
+-- save to unix clipboard by default
+-- opt.clipboard = 'unnamedplus'
 
-opt.clipboard = 'unnamedplus'
+-- tabs width
+opt.tabstop = 4
+opt.shiftwidth = 4
 
+-- save indent length
+opt.breakindent = true
 
+opt.cin = true
